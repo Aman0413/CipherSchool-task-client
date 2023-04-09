79a06266347a21c4425542971432db6a29138314
Aman Verma <aman@Amans-MacBook-Air.local>
2023-04-09 19:25:59 +0530

    make deploy ready

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let baseURL = "http://localhost:4000";
-if (process.env.NODE) {
+if (process.env.NODE === "production") {
   baseURL = process.env.REACT_APP_BASE_URL;
 }
 export const axiosClient = axios.create({
